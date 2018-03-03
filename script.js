9e12dc39d425dc71d80980585a9e85d8e089ecbc
AoiBurizado <aoi_burizado@hotmail.com>
2018-03-03 13:11:33 +0100

    fixed ids

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 
 
-var xSend = document.getElementById(send);
-var xReceive = document.getElementById(receive);
-var xHistory = document.getElementById(history);
+var xSend = document.getElementById("send");
+var xReceive = document.getElementById("receive");
+var xHistory = document.getElementById("history");
+var buttonNavDiv = document.getElementById("navbuttonsdiv");
 
 function sendPage() {
     xSend.style.display = "block";
     xReceive.style.display = "none";
     xHistory.style.display = "none";
+    buttonNavDiv.style.display = "block";
 }
 
 function receivePage() {
     xSend.style.display = "none";
     xReceive.style.display = "block";
     xHistory.style.display = "none";
+    buttonNavDiv.style.display = "block";
 }
 
 function historyPage() {
     xSend.style.display = "none";
     xReceive.style.display = "none";
     xHistory.style.display = "block";
+    buttonNavDiv.style.display = "block";
 }
 
 function chooseUser() {
-    
+    buttonNavDiv.style.display = "none";
 }
 
 
