e34cb287dece5117036ef315b2efa63d3f5c4b88
AoiBurizado <aoi_burizado@hotmail.com>
2018-03-03 12:29:10 +0100

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 
+
+var xSend = document.getElementById(send)
+var xReceive = document.getElementById(receive)
+var xHistory = document.getElementById(history)
+
+function sendPage() {
+    xSend.style.display = "block"
+    xReceive.style.display = "none"
+    xHistory.style.display = "none"
+}
+
+function receivePage() {
+    xSend.style.display = "none"
+    xReceive.style.display = "block"
+    xHistory.style.display = "none"
+}
+
+function historyPage() {
+    xSend.style.display = "none"
+    xReceive.style.display = "none"
+    xHistory.style.display = "block"
+}
+
 function chooseUser() {
     
 }
