eb4287b8c6c91e34aa6db629cc0762397169a763
larshgryt <larshgrytten@gmail.com>
2018-03-03 15:48:48 +0100

    First step API

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-
-var xSend = document.getElementById(send)
+var xhr = new XMLHttpRequest();
+/*var xSend = document.getElementById(send)
 var xReceive = document.getElementById(receive)
 var xHistory = document.getElementById(history)
 
@@ -20,11 +20,12 @@ function historyPage() {
     xSend.style.display = "none"
     xReceive.style.display = "none"
     xHistory.style.display = "block"
-}
+}*/
 
 function chooseUser() {
-    
+    xhr.open("GET", "https://dnbapistore.com/hackathon/customers/3.0/customer/01011900123", true);
+    xhr.setRequestHeader('Authorization', "Bearer 3274443e-de83-39b3-8087-2f17fe4e02ea");
 }
-
+window.onload = chooseUser();
 
 
