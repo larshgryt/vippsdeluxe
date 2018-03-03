7aba0bb72f831a064b8da1115c4ec03f4165106e
AoiBurizado <aoi_burizado@hotmail.com>
2018-03-03 12:55:10 +0100

    semicolon

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 
 
-var xSend = document.getElementById(send)
-var xReceive = document.getElementById(receive)
-var xHistory = document.getElementById(history)
+var xSend = document.getElementById(send);
+var xReceive = document.getElementById(receive);
+var xHistory = document.getElementById(history);
 
 function sendPage() {
-    xSend.style.display = "block"
-    xReceive.style.display = "none"
-    xHistory.style.display = "none"
+    xSend.style.display = "block";
+    xReceive.style.display = "none";
+    xHistory.style.display = "none";
 }
 
 function receivePage() {
-    xSend.style.display = "none"
-    xReceive.style.display = "block"
-    xHistory.style.display = "none"
+    xSend.style.display = "none";
+    xReceive.style.display = "block";
+    xHistory.style.display = "none";
 }
 
 function historyPage() {
-    xSend.style.display = "none"
-    xReceive.style.display = "none"
-    xHistory.style.display = "block"
+    xSend.style.display = "none";
+    xReceive.style.display = "none";
+    xHistory.style.display = "block";
 }
 
 function chooseUser() {
